e8f4de79fc8c7118f3ec05a047fa36e8b8fbe594
Tnhnyldz <tunahanyildiz1560@gmail.com>
2022-06-16 23:30:51 +0300

    blogs

--- MvcProje/Controllers/BlogController.cs
+++ MvcProje/Controllers/BlogController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,13 +10,15 @@ namespace MvcProje.Controllers
     public class BlogController : Controller
     {
         // GET: Blog
+        BlogManager bm = new BlogManager();
         public ActionResult Index()
         {
             return View();
         }
         public PartialViewResult BlogList()
         {
-            return PartialView();
+            var bloglist = bm.GetAll();
+            return PartialView(bloglist);
         }
         public PartialViewResult FeaturedPosts()
         {
