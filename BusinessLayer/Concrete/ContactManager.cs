bc731a66c266ae9625d7543e9f8ff046caa376f9
Tnhnyldz <tunahanyildiz1560@gmail.com>
2022-07-01 02:19:43 +0300

    Dark theme and author section

---
+++ BusinessLayer/Concrete/ContactManager.cs
@@ -0,0 +1,23 @@
+﻿using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class ContactManager
+    {
+        Repository<Contact> repoContact = new Repository<Contact>();
+        public int BLContactAdd(Contact c)
+        {
+            if (c.Mail=="" || c.Message=="" || c.Name=="" || c.Subject=="" || c.SurName=="")
+            {
+                return -1;
+            }
+            return repoContact.Insert(c);
+        }
+    }
+}
